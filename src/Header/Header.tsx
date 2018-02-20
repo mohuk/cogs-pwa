ce9d3e476b063720f52b75787e9f2a42f1ab7614
asjadsaboor <asjad.saboor@tenpearls.com>
2018-02-20 13:10:44 +0500

    feat(login): add login form

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -2,14 +2,10 @@ import * as React from 'react';
 import { push } from 'react-router-redux';
 import { bindActionCreators, Dispatch } from 'redux';
 import { connect } from 'react-redux';
-// import { AppBar } from 'material-ui';
-// import './Login.css';
+import { AppBar } from 'material-ui';
+
 export const Header = (props: any) => (
-  <div>
-    <h1>Home</h1>
-    <p>Welcome home!</p>
-    <button onClick={() => props.changePage()}>Go to about page via redux</button>
-  </div>
+  <AppBar title="COGS - Generation NEXT" iconClassNameRight="muidocs-icon-navigation-expand-more"/>
 );
 
 // export class Header extends React.Component {
--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,19 +1,53 @@
+import { RaisedButton, TextField } from 'material-ui';
+import  './Login.css';
+// import axios from 'axios';
 import * as React from 'react';
-import './Login.css';
+interface LoginProps {
 
-export class Login extends React.Component {
+}
+
+interface LoginState {
+  username: string;
+  password: string;
+}
+
+const style = {
+  margin: 15,
+};
+
+export class Login extends React.Component<LoginProps, LoginState> {
+  constructor(props: LoginProps) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  handleClick() {
+    // var apiBaseUrl = 'https://cogs.10pearls.com/cogsapi/api/auth/login';
+
+    // console.log('inside handleclick', this.state);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div className="login-container">
+        <TextField
+          hintText="Enter your Username"
+          floatingLabelText="Username"
+          onChange={(event, newValue) => this.setState({ username: newValue })}
+        />
+        <br />
+        <TextField
+          type="password"
+          hintText="Enter your Password"
+          floatingLabelText="Password"
+          onChange={(event, newValue) => this.setState({ password: newValue })}
+        />
+        <br />
+        <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick()} />
       </div>
     );
   }
 }
-
-export default Login;
