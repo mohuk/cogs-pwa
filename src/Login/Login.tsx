25735f68aab062dfcf0317fb8c18d10758df666b
Mohammad Umair Khan <mohammad.umair.k@gmail.com>
2018-02-20 11:52:19 +0500

    feat(lunch): add lunch

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -3,16 +3,16 @@ import './Login.css';
 
 class App extends React.Component {
   render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Welcome to React</h1>
-      </header>
-      <p className="App-intro">
-        To get started, edit <code>src/App.tsx</code> and save to reload.
-      </p>
-    </div>
-  );
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </div>
+    );
   }
 }
 
